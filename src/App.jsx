ed5dcb3e33d7bb79491b96c44f5d89cc47d7730c
Neha C J <cjneha890@gmail.com>
2025-06-09 20:40:29 +0530

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -40,25 +40,24 @@ function App() {
 
       <h1 className="text-7xl tracking-[0.2em] text-center mt-10">PORTFOLIO</h1>
 
-      <div className="flex flex-col items-center justify-center mt-20 px-4">
+      <section className="flex flex-col items-center justify-center mt-20 mb-40 px-4">
+        <h2 className="text-4xl py-10" >Hey, This is Neha</h2>
 
-        <div className="max-w-xl text-xl text-center md:text-left">
-          <p>Hey this is Neha! I am passionate about FrontEnd WebDevelopment and problem solving.</p>
+        <div className="max-w-xl text-center md:text-left">
+          <p>I am passionate about FrontEnd WebDevelopment and problem solving.</p>
           <br/>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-            standard dummy text ever since the 1500s.
-            <br/>Lorem Ipsum is simply dummy text.
+          <p>I’m a student who enjoys coding and learning new things. 
+            I like working on projects that help me grow and challenge myself. Always open to collaboration and new opportunities. 
           </p>
         </div>
-      </div>
+      </section>
 
       <div className="px-6 py-12 flex flex-col md:flex-row items-center justify-center gap-10">
         
         <div className="text-xl max-w-2xl">
-          <h2 className="text-3xl md:text-4xl font-bold mb-6 ">ABOUT ME</h2>
-            <p className="text-lg mb-4"> I am a BTech student currently pursuing Computer Science Engineering
-            in Model Engineering College, Thrikkakara
+          <h2 className="text-3xl md:text-4xl font-bold mb-6 ">About Me</h2>
+            <p className="text-lg mb-4"> I am currently pursuing a BTech degree in Computer Science Engineering at Model Engineering College, Thrikkakara.
+              I aspire to contribute to meaningful software solutions and keep evolving as a developer.
             </p>
             <p className="mb-4">Here are some technologies I have been working with:</p>
             <div className=" text-xl grid grid-cols-2 gap-x-10 gap-y-2 mb-6 list-disc list-inside">
@@ -72,7 +71,7 @@ function App() {
             <p className="text-sm">I also love Reading Books, Learning French and playing Acoustic Guitar (self taught)</p>
           </div>
           
-          <div className="flex-shrink-0">
+          <div className="flex-shrink-0 transition-transform duration-500 relative hover:scale-105">
             <img
                 src="bali.jpg"
                 alt=" dummy Profile"
@@ -88,7 +87,7 @@ function App() {
         {projects.map((project, index) => (
           <div
             key={index}
-            className="bg-[#261a56] p-6 rounded-xl shadow hover:shadow-xl transition-all duration-300 relative"
+            className="bg-[#261a56] p-6 rounded-xl shadow hover:shadow-xl transition-all duration-500 relative ransition-transform hover:scale-105"
           >
             <div className="flex justify-between items-center mb-4">
               <BsFolder2Open className="text-2xl" />
@@ -109,6 +108,9 @@ function App() {
         ))}
       </div>
     </section>
+    <footer className=" flex flex-row items-center justify-center">
+        <p>Created and designed by Neha C J</p>
+    </footer>
       
     </>
   );
